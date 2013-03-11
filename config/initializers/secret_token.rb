e450c532eb23e44d45647c36f53724d681baba70
snake <snake@beef.nisra.net>
2013-03-11 17:54:59 +0800

    Backup Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = 'd5a3054f341336ff7ca393cfe0633a5ba79e94ea6dca7b0b13b01cf39bf5e8698aa9d1ce592613d9fae911259fbc4cb5adc18bda284135269ddc70dafcff94bf'
