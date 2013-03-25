b308778fcdd5a4c795882fc8a3a26813de768f8a
Adnil Lee <lynx@nisra.net>
2013-03-25 19:09:29 +0800

    Backup Project

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,10 +4,16 @@ class EventsController < ApplicationController
   end
   def new
     @event = Event.new
+    @option = params[:option]
+    @count = params[:count]
+    @level = params[:level]
   end
   def create
     @a = params[:a]
     @q = params[:q]
     @c = params[:c]
+    @option = params[:option]
+    @count = params[:count]
+    @level = params[:level]
   end
 end
