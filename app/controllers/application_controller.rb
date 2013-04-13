8b553967ed0ac930aa707b94a229740cb1b4fb11
Adnil Lee <lynx@nisra.net>
2013-04-13 10:17:25 +0800

    Backup Project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  def ope(a, b, o)
+    case o
+    when 0
+      c = a + b
+    when 1
+      c = a - b
+    when 2
+      c = a * b
+    when 3
+      t = a * b
+      c = a
+      a = t
+      return c
+    end
+  end
 end
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,6 +5,53 @@ class EventsController < ApplicationController
     @option = params[:option]
     @count = params[:count]
     @level = params[:level]
+    @level[:level] = (@level[:level].to_i < 1) ? 1 : @level[:level].to_i
+    nu = @option[:nu].to_i
+    @q = Array.new(nu)
+    @c = Array.new(nu)
+    (1...nu).each do |e|
+      puts e
+      @a = rand(10**(@level[:level]-1)...10**@level[:level])
+      @b = rand(10**(@level[:level]-1)...10**@level[:level])
+      @o = @option[:type] == "4" ? rand(0..3) : @option[:type].to_i
+      @op = ['+', '-', '*', '/' ]
+      @option[:arc] = @option[:arc] ? @option[:arc].to_i : @option[:ar].to_i
+      if @option[:arc] == 2
+        @d = rand(10**(@level[:level]-1)...10**@level[:level])
+        @o2 = @option[:type] == "4" ? rand(0..3) : @option[:type].to_i
+        @pri = rand(0..1)
+        case @pri
+        when 0
+          @o2 = @o2 == 3 ? rand(0..2) : @o2
+          @c[e] = ope(ope(@a, @b, @o), @d, @o2)
+          case @o
+          when 2
+            @q[e] = "#{@a} #{@op[@o]} #{@b} #{@op[@o2]} #{@d} ="
+          when 3
+            @q[e] = "#{ope(@a, @b, 2)} #{@op[@o]} #{@b} #{@op[@o2]} #{@d} ="
+          else
+            @q[e] = @o2 > 1 ? "(#{@a} #{@op[@o]} #{@b}) #{@op[@o2]} #{@d} =" : "#{@a} #{@op[@o]} #{@b} #{@op[@o2]} #{@d} ="
+          end
+        when 1
+          @o = @o == 3 ? rand(0..2) : @o
+          @c[e] = ope(@a, ope(@b, @d, @o2), @o)
+          case @o2
+          when 3
+            @q[e] = "#{@a} #{@op[@o]} (#{ope(@b, @d, 2)} #{@op[@o2]} #{@d}) ="
+          else
+            @q[e] = @o == 2 ? "#{@a} #{@op[@o]} #{@b} #{@op[@o2]} #{@d} =" : "#{@a} #{@op[@o]} (#{@b} #{@op[@o2]} #{@d}) ="
+          end
+        end
+      else
+        @q[e] = "#{@a} #{@op[@o]} #{@b} ="
+        @c[e] = ope(@a, @b, @o)
+      end
+    end
+    if @option[:arc] == 2
+      @option[:arc] = @option[:arc] - 1
+    else
+      @option[:arc] = @option[:arc] == 1 ? @option[:arc] + 1 : @option[:arc]
+    end
   end
   def create
     @a = params[:a]
@@ -13,5 +60,27 @@ class EventsController < ApplicationController
     @option = params[:option]
     @count = params[:count]
     @level = params[:level]
+    @r = 0
+    @nu = @option[:nu].to_i
+    @c = Array.new(@nu)
+    (1...@nu).each do |e|
+      @s = e.to_s
+      puts @a[@s]
+      if @a[@s] != "" && (@a[e].to_i == @c[e].to_i)
+        @r += 1
+        @c[e] = true
+      else
+        @c[e] = false
+      end
+    end
+    @count[:count] = @count[:count].to_i + @nu - 1
+    @count[:correct] = @count[:correct].to_i + @r
+    cc = (@count[:correct].to_f / @count[:count].to_f)
+    case 
+    when cc > 0.8
+      @level[:level] = @level[:level].to_i + 1
+    when cc < 0.2
+      @level[:level] = @level[:level].to_i - 1
+    end
   end
 end
